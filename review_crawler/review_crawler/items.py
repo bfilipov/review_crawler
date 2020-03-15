69b2faa53556b872766fea081f27caa861e43b32
Bogomil Filipov <b.filipov@browswave.com>
2020-03-15 14:32:12 +0200

    upd

--- review_crawler/review_crawler/items.py
+++ review_crawler/review_crawler/items.py
@@ -6,12 +6,21 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
 
 
 class ProductItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field(output_processor=Join)
+    article_id = scrapy.Field(output_processor=TakeFirst)
+    images = scrapy.Field()
+    url = scrapy.Field(output_processor=TakeFirst)
+    list_page = scrapy.Field(output_processor=TakeFirst)
+    price = scrapy.Field(output_processor=TakeFirst)
+    stock = scrapy.Field(output_processor=TakeFirst)
+    currency = scrapy.Field(output_processor=TakeFirst)
+    description = scrapy.Field(output_processor=Join)
+    vendor_id = scrapy.Field(output_processor=TakeFirst)
 
 
 class ReviewItem(scrapy.Item):
--- review_crawler/review_crawler/spiders/jd.py
+++ review_crawler/review_crawler/spiders/jd.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import re
 import scrapy
 from review_crawler.items import ProductItem, ReviewItem
 from scrapy.loader import ItemLoader
 
 class JdSpider(scrapy.Spider):
     name = 'jd'
-    allowed_domains = ['jd.com']
+    allowed_domains = ['jd.com', '3.cn']
     start_urls = [
         'https://list.jd.com/list.html?cat=670,671,2694&page=1&sort=sort_totalsales15_desc&trans=1&JL=6_0_0/']
 
@@ -23,11 +24,47 @@ class JdSpider(scrapy.Spider):
     def parse_product(self, response):
         meta = response.meta
         loader = ItemLoader(ProductItem(), selector=response)
-        import ipdb
-        ipdb.set_trace()
+        url = response.url
+        article_id = self.extract_id_from_url(url)
+        if not article_id:
+            return
+        loader.add_value('article_id', article_id)
+        loader.add_value('url', url)
+        loader.add_value('list_page', meta.get('list_page', ''))
+        loader.add_css('images', '.lh li img::attr(src)')
+        loader.add_css('title', '.sku-name::text')
+        loader.add_css('currency', '.p-price span::text')
+        loader.add_css('description', '.parameter2.p-parameter-list *::text')
+        vendor_id = self.get_vendor_id(response)
+        loader.add_value('vendor_id', vendor_id)
+
+        yield self.get_stock_and_price(loader)
+        import ipdb; ipdb.set_trace()
+        # price = scrapy.Field()
+        # stock = scrapy.Field()
 
     def parse_review(self, response):
         # get summary of comments (GET METHOD)
         # https://club.jd.com/ProductPageService.aspx?method=GetCommentSummaryBySkuId&referenceId=100002716279
         import ipdb
         ipdb.set_trace()
+
+    @staticmethod
+    def extract_id_from_url(url):
+        art_id = re.search(r'jd\.com\/(\d.*?)\.html', url, re.IGNORECASE).grpup(1)
+        return art_id
+
+    @staticmethod
+    def get_vendor_id(response):
+        body = response.css('script[charset=gbk]::text').get()
+        match = re.search(r'venderId:\s?(.*?),', body, re.IGNORECASE)
+        return match.group(1) if match else ''
+
+    def get_stock_and_price(self, loader):
+        art_id = loader.get_collected_values('article_id')
+        vendor_id = loader.get_collected_values('vendor_id')
+        cat = loader.get_collected_values('list_page')
+        f'https://c0.3.cn/stock?skuId={art_id}&area=53283_53362_0_0&venderId={vendor_id}&buyNum=1\
+        &choseSuitSkuIds=&cat={cat}'
+
+    def parse_stock_and_price(self, response):
